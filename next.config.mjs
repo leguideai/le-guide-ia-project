df965161f143bba36aed2ad47910b7389a19282e
99mass <diopsamba408@gmail.com>
2026-06-10 17:35:10 +0000

    Refactor Cloudflare initialization to be conditional based on environment variable

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
-import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
-
-initOpenNextCloudflareForDev();
+if (process.env.OPENNEXT_ENABLE_CLOUDFLARE_DEV === "true") {
+  const { initOpenNextCloudflareForDev } = await import("@opennextjs/cloudflare")
+  initOpenNextCloudflareForDev()
+}
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
